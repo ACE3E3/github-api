9b6ec1cf5f68748bd6514d991dbbcf09c70b3621
ACE3E3 <53176174+ACE3E3@users.noreply.github.com>
2023-05-27 13:01:20 +0530

    Update Api.jsx

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import * as reactStore from "../store/actions/action";
 import ShowProfile from "./ShowProfile";
